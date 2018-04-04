1a93ef7f0d23f0dbb7002286d72b64e09f55eb9e
Babajide Williams <babanila@yahoo.com>
2018-04-04 16:40:26 +0200

    k

--- es6FileUpload.js
+++ es6FileUpload.js
@@ -8,21 +8,20 @@ const url  = require('url');
 const server = http.createServer((req, res) => {
     if (req.url == '/fileupload') {
         let form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
+        form.parse(req, (err, fields, files) => {
             const oldPath = files.filetoupload.path;
             const newPath = '/Users/becky/Desktop/Test/' + files.filetoupload.name;
-            fs.rename(oldPath, newPath, function(err){
+            fs.rename(oldPath, newPath, (err) => {
                 if(err) throw err;
-                res.write('Your File have been Uploaded and Renamed');
+                res.write('Your File have been Uploaded');
                 res.end();
             });
         });
     } else {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
+        res.write(`<form action="fileupload" method="post" enctype="multipart/form-data"> 
+                    <input type="file" name="filetoupload"><br> 
+                    <input type="submit"> </form>`);
         return res.end();
     }
 })
