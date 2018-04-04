022373b1d2741b3cae7362d54b5dd5fa3f05d324
Babajide Williams <babanila@yahoo.com>
2018-04-04 16:27:34 +0200

    Add files via upload

---
+++ es6FileUpload.js
@@ -0,0 +1,29 @@
+
+const http  = require('http');
+const formidable = require('formidable');
+const fs  = require('fs');
+const url  = require('url');
+
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/fileupload') {
+        let form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            const oldPath = files.filetoupload.path;
+            const newPath = '/Users/becky/Desktop/Test/' + files.filetoupload.name;
+            fs.rename(oldPath, newPath, function(err){
+                if(err) throw err;
+                res.write('Your File have been Uploaded and Renamed');
+                res.end();
+            });
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+})
+    server.listen(8080);
