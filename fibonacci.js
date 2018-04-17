526c2ebd68045406e686afc1c95088b8f03ce02d
Babajide Williams <babanila@yahoo.com>
2018-04-17 21:44:04 +0200

    k

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+// Fibonacci Series
+const fibonacci = num => {
+    if(num < 0) return 0;
+    if (num <= 1) return 1;
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(7));
+console.log(fibonacci(21));
